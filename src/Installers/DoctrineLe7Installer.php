099faca97475649d287ce11813745904b4fd2a45
rnr1721 <admin@ehukr.com>
2023-11-26 16:38:40 +0200

    + installer fixes

---
+++ src/Installers/DoctrineLe7Installer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Installers;
+
+class DoctrineLe7Installer
+{
+
+    public function copyConfig()
+    {
+        $sourceConfigFile = __DIR__ . '/dist/db_doctrine.php';
+        $sourceContainerConfigFile = __DIR__ . '/dist/dbDoctrineContainerConf.php';
+        $destinationConfigDirectory = __DIR__ . '/../../../config';
+        $destinationContainerDirectory = __DIR__ . '/../../../container';
+        $destinationConfigPath = $destinationConfigDirectory . '/db_doctrine.php';
+        $destinationContainerConfigPath = $destinationContainerDirectory . '/dbDoctrineContainerConf.php';
+        if (!file_exists($destinationConfigPath)) {
+            copy($sourceConfigFile, $destinationConfigPath);
+        }
+        if (!file_exists($destinationContainerConfigPath)) {
+            copy($sourceContainerConfigFile, $destinationContainerConfigPath);
+        }
+    }
+}
